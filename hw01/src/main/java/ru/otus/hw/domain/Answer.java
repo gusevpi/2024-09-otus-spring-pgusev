fedf5275bb103ac4448f6a3a4b9e98624b6e81d4
Pavel Gusev <pigusev@beeline.ru>
2024-10-06 17:48:23 +0300

    Переопределен метод toString для красивого вывода в консоль вопроса и вариантов ответа

--- hw01/src/main/java/ru/otus/hw/domain/Answer.java
+++ hw01/src/main/java/ru/otus/hw/domain/Answer.java
@@ -1,4 +1,9 @@
 package ru.otus.hw.domain;
 
 public record Answer(String text, boolean isCorrect) {
+
+    @Override
+    public String toString() {
+        return "*   %s".formatted(this.text);
+    }
 }
