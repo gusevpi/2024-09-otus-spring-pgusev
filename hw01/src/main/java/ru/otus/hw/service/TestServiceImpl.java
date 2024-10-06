f7313938e962fa6c24f2a5e8f619e9cbe28b76ec
Pavel Gusev <pigusev@beeline.ru>
2024-10-06 18:19:53 +0300

    Убраны прямые зависимости

--- hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,8 +1,6 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
-import ru.otus.hw.config.AppProperties;
-import ru.otus.hw.dao.CsvQuestionDao;
 import ru.otus.hw.dao.QuestionDao;
 import ru.otus.hw.domain.Question;
 
@@ -13,12 +11,13 @@ public class TestServiceImpl implements TestService {
 
     private final IOService ioService;
 
+    private final QuestionDao questionDao;
+
     @Override
     public void executeTest() {
         ioService.printLine("");
         ioService.printFormattedLine("Please answer the questions below%n");
         // Получить вопросы из дао и вывести их с вариантами ответов
-        QuestionDao questionDao = new CsvQuestionDao(new AppProperties("/questions.csv"));
         List<Question> questionList = questionDao.findAll();
         questionList.forEach(row -> ioService.printFormattedLine(row.toString()));
     }
