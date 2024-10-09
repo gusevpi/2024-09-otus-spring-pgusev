01f7333c7aecb0518756cdbca6e7a55b66789db6
Pavel Gusev <pigusev@beeline.ru>
2024-10-10 01:36:52 +0300

    Добавлена возможность запуска из jar

--- hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -2,35 +2,32 @@ package ru.otus.hw.dao;
 
 import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
-import ru.otus.hw.Application;
 import ru.otus.hw.config.TestFileNameProvider;
 import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
 import ru.otus.hw.exceptions.QuestionReadException;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.Reader;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.Objects;
 
 @RequiredArgsConstructor
 public class CsvQuestionDao implements QuestionDao {
 
     private final TestFileNameProvider fileNameProvider;
 
+    @SuppressWarnings("checkstyle:NeedBraces")
     @Override
     public List<Question> findAll() {
         List<QuestionDto> questionRows;
-        try {
-            // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
-            URL url = Objects.requireNonNull(Application.class.getResource(fileNameProvider.getTestFileName()));
-            Path resourcePath = Paths.get(url.toURI());
-            Reader reader = Files.newBufferedReader(resourcePath);
+        try (InputStream is = getClass().getResourceAsStream(fileNameProvider.getTestFileName())) {
+            if (is == null) {
+                throw new QuestionReadException("File not found");
+            }
+            Reader reader = new BufferedReader(new InputStreamReader(is));
 
             // Использовать CsvToBean
             // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
@@ -40,7 +37,7 @@ public class CsvQuestionDao implements QuestionDao {
                     .withSkipLines(1)
                     .build()
                     .parse();
-        } catch (URISyntaxException | IOException e) {
+        } catch (IOException e) {
             // Использовать QuestionReadException
             throw new QuestionReadException(e.getMessage());
         }
