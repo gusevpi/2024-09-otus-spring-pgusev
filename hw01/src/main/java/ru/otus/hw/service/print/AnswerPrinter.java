0fd1c6afedf7f4a198761f1587468690240ba389
Pavel Gusev <pigusev@beeline.ru>
2024-10-10 23:20:14 +0300

    Добавлен вывод вариантов ответов с нумерацией

--- hw01/src/main/java/ru/otus/hw/service/print/AnswerPrinter.java
+++ hw01/src/main/java/ru/otus/hw/service/print/AnswerPrinter.java
@@ -1,21 +1,28 @@
 package ru.otus.hw.service.print;
 
 import lombok.experimental.UtilityClass;
+import org.apache.commons.lang3.StringUtils;
 import ru.otus.hw.domain.Answer;
 
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @UtilityClass
 public class AnswerPrinter implements PrintService {
 
     public static String format(List<Answer> answers) {
-        String formattedAnswers = answers.stream()
-                .filter(Objects::nonNull)
-                .map(AnswerPrinter::format)
+        return IntStream.range(0, answers.size())
+                .mapToObj(index -> format(index + 1, answers.get(index)))
                 .collect(Collectors.joining("\n"));
-        return formattedAnswers;
+    }
+
+    public static String format(int index, Answer answer) {
+        if (Objects.isNull(answer)) {
+            return StringUtils.EMPTY;
+        }
+        return "%d   %s".formatted(index, answer.text());
     }
 
     public static String format(Answer answer) {
