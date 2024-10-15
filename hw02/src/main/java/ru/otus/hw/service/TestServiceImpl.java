b2498238cf5064cce57e02ac323840f19ee552ae
Pavel Gusev <pigusev@beeline.ru>
2024-10-16 00:07:54 +0300

    Добавлена логика вывода и считывания вопроса, а так же проверка валидности ответа

--- hw02/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw02/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,18 +1,32 @@
 package ru.otus.hw.service;
 
-import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
+import ru.otus.hw.service.print.QuestionPrinter;
 
-@RequiredArgsConstructor
+import java.util.List;
+
 @Service
 public class TestServiceImpl implements TestService {
 
     private final IOService ioService;
 
     private final QuestionDao questionDao;
+
+    private final QuestionPrinter questionPrinter;
+
+    public TestServiceImpl(IOService ioService,
+                           QuestionDao questionDao,
+                           @Qualifier("questionPrinter") QuestionPrinter questionPrinter) {
+        this.ioService = ioService;
+        this.questionDao = questionDao;
+        this.questionPrinter = questionPrinter;
+    }
 
     @Override
     public TestResult executeTestFor(Student student) {
@@ -21,10 +35,25 @@ public class TestServiceImpl implements TestService {
         var questions = questionDao.findAll();
         var testResult = new TestResult(student);
 
-        for (var question: questions) {
-            var isAnswerValid = false; // Задать вопрос, получить ответ
+        for (var question : questions) {
+            String userAnswerInput = ioService.readStringWithPrompt(questionPrinter.format(question));
+            String correctAnswerIndex = fetchCorrectAnswerIndex(question.answers());
+            var isAnswerValid = StringUtils.isNotBlank(correctAnswerIndex) && userAnswerInput.equals(correctAnswerIndex);
             testResult.applyAnswer(question, isAnswerValid);
         }
         return testResult;
+    }
+
+    private String fetchCorrectAnswerIndex(List<Answer> answers) {
+        if (answers == null) {
+            return StringUtils.EMPTY;
+        }
+        for (int i = 0; i < answers.size(); i++) {
+            Answer answer = answers.get(i);
+            if (answer != null && answer.isCorrect()) {
+                return String.valueOf(i + 1);
+            }
+        }
+        return StringUtils.EMPTY;
     }
 }
