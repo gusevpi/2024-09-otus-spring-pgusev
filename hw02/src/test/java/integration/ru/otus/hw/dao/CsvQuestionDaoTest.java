869aa71f11df63c30a11f5583b8075838eda0f4a
Pavel Gusev <pigusev@beeline.ru>
2024-10-16 22:02:59 +0300

    Добавлен второй вариант теста с заглушкой

--- hw02/src/test/java/integration/ru/otus/hw/dao/CsvQuestionDaoTest.java
+++ hw02/src/test/java/integration/ru/otus/hw/dao/CsvQuestionDaoTest.java
@@ -1,29 +1,29 @@
-package ru.otus.hw.dao;
+package integration.ru.otus.hw.dao;
 
+import integration.ru.otus.hw.config.AppPropertiesTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mockito;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import ru.otus.hw.config.AppPropertiesTest;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.CsvQuestionDao;
+import ru.otus.hw.exceptions.QuestionReadException;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {AppPropertiesTest.class})
 @TestPropertySource(locations = "/application.properties")
 class CsvQuestionDaoTest {
 
-    @Autowired
-    TestFileNameProvider fileNameProvider;
-
     @Test
-    @DisplayName("Чтение из тестового ресурса")
-    void findAll_test_1() {
+    @DisplayName("Чтение из несуществующего ресурса c помощью заглушки")
+    void findAll_test_2() {
+        TestFileNameProvider fileNameProvider = Mockito.mock(TestFileNameProvider.class);
+        Mockito.when(fileNameProvider.getTestFileName()).thenReturn("notFound.csv");
         CsvQuestionDao csvQuestionDao = new CsvQuestionDao(fileNameProvider);
-        Assertions.assertDoesNotThrow(csvQuestionDao::findAll);
-        Assertions.assertEquals(6, csvQuestionDao.findAll().size());
+        Assertions.assertThrowsExactly(QuestionReadException.class, csvQuestionDao::findAll);
     }
 }
