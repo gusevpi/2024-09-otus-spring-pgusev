19b01a4cf0ca9611f714570c6dc94b2153c6b95e
Pavel Gusev <pigusev@beeline.ru>
2024-10-19 15:34:20 +0300

    В тесте на моках удален контекст

--- hw02/src/test/java/integration/ru/otus/hw/dao/CsvQuestionDaoTest.java
+++ hw02/src/test/java/integration/ru/otus/hw/dao/CsvQuestionDaoTest.java
@@ -1,21 +1,14 @@
 package integration.ru.otus.hw.dao;
 
-import integration.ru.otus.hw.config.AppPropertiesTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.otus.hw.config.TestFileNameProvider;
 import ru.otus.hw.dao.CsvQuestionDao;
 import ru.otus.hw.exceptions.QuestionReadException;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = {AppPropertiesTest.class})
-@TestPropertySource(locations = "/application.properties")
+
 class CsvQuestionDaoTest {
 
     @Test
