a1f1e20b4e8d99b23e83cf9d89fd1d9c1ce954ec
Pavel Gusev <pigusev@beeline.ru>
2024-10-27 15:36:05 +0300

    Добавлен запуск приглашения к вводу после старта приложения

--- hw03/src/main/java/ru/otus/hw/Application.java
+++ hw03/src/main/java/ru/otus/hw/Application.java
@@ -1,11 +1,23 @@
 package ru.otus.hw;
 
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.otus.hw.service.TestRunnerService;
 
+@AllArgsConstructor
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+    private final TestRunnerService testRunnerService;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        testRunnerService.run();
     }
 }
