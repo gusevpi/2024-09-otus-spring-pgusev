6cf96f91fa680ae1ad257f8009b16159b85b04cf
Pavel Gusev <pigusev@beeline.ru>
2024-11-03 22:39:24 +0300

    Имплементация CommandLineRunner вынесена из Application.java

--- hw03/src/main/java/ru/otus/hw/Application.java
+++ hw03/src/main/java/ru/otus/hw/Application.java
@@ -1,23 +1,14 @@
 package ru.otus.hw;
 
 import lombok.AllArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.otus.hw.service.TestRunnerService;
 
 @AllArgsConstructor
 @SpringBootApplication
-public class Application implements CommandLineRunner {
-
-    private final TestRunnerService testRunnerService;
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    public void run(String... args) {
-        testRunnerService.run();
     }
 }
