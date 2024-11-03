202868f7f0ce438d709160604859dc31978b97b9
Pavel Gusev <pigusev@beeline.ru>
2024-11-03 22:42:51 +0300

    Удалена лишняя аннотация

--- hw03/src/main/java/ru/otus/hw/Application.java
+++ hw03/src/main/java/ru/otus/hw/Application.java
@@ -3,9 +3,11 @@ package ru.otus.hw;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @AllArgsConstructor
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class Application {
 
     public static void main(String[] args) {
